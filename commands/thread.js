7cbbb0352f5591815764c3fc60511394bdfeb15f
BluefireCrystal1 <bluefirecrystal@outlook.com>
2022-02-04 09:55:12 +0530

    Co-authored-by: Beluwuuga <Beluwuuga@users.noreply.github.com>

--- commands/thread.js
+++ commands/thread.js
@@ -7,34 +7,34 @@ module.exports = {
     name: 'thread',
     description: helpList.thread.value,
     async execute(message, ...args){
-        if(message.channel.id === chnlId){
-        message.reply(`Creating....`).then(async rslt =>{
-            const ping = rslt.createdTimestamp - message.createdTimestamp
 
+        if(message.channel.id!=chnlId) return message.channel.send(
+            `The thread feature is only for help with bot code, goto <#${chnlId}> to create a thread`)
+        message.reply(`Creating....`).then(async rslt =>{
+            
+            const members = ['815121233057808424','902922671589830686','880313471206588428'];
+            const ping = rslt.createdTimestamp - message.createdTimestamp
+            const commandLength = 7
             await rslt.edit(`Creating...`)
             await rslt.edit(`Creating..`)
             await rslt.edit(`Creating.`)
-            
+            const threadName = message.content.slice(commandLength,message.length) 
             
             const thread = await message.channel.threads.create({
                 
-                name: message.content.replace("?thread", '').replace("?Thread", '').replace("?THREAD", ''),
+                name: threadName,
                 autoArchiveDuration: 1440,
-                reason: '-',
+                reason: 'Someone asked for help',
             });
-            
-            await thread.members.add('815121233057808424');
-            await thread.members.add('902922671589830686');
-            await thread.members.add('880313471206588428');
-            console.log(`Created thread: ${thread.name}`);
+            members.forEach(async element => { 
+                await thread.members.add(element);
+            });console.log(`Created thread: ${thread.name}`);
             await rslt.edit(`Done!`)
             await rslt.delete()
-            await thread.send(`${message.member.displayName} Needs help with: ${message.content.replace("?thread", '').replace("?Thread", '').replace("?THREAD", '')}`)
-            await thread.send(`Here are some important things you need to know with help threads: \n 1) Do not ping everyone or anyone in the thread because they already get pinged. \n 2) Wait for their response, have patience`)
+            await thread.send(`${message.member.displayName} Needs help with: ${threadName}`)
+            await thread.send(`Here are some important things you need to know with help threads: 
+            \n 1) Do not ping everyone or anyone in the thread because they already get pinged. 
+            \n 2) Wait for their response, have patience`)
         })
-
-        }else {
-            return
-        }
     }
 }
