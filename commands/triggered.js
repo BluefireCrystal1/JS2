45e8fda9e7a81ed6bbda527da5a13ae97e3cd6b3
BluefireCrystal1 <bluefirecrystal@outlook.com>
2022-02-10 15:12:23 +0530

    init

--- commands/triggered.js
+++ commands/triggered.js
@@ -7,7 +7,9 @@ module.exports = {
     name: 'triggered',
     description: helpList.triggered.value,
     async execute(message, ...args) {
-        const member = message.mentions.users.first() || message.author
+        const member = message.mentions.users.first() || message.author;
+        const bluefire = client.users.cache.find(user => user.id === '880313471206588428')
+        if(member === bluefire) return message.channel.send('You cannot run this command on that member');
         let avatar = member.displayAvatarURL({ dynamic: false, format: 'png' });
         let image = await canvacord.Canvas.trigger(avatar);
         let attachment = new MessageAttachment(image, "triggered.gif");
